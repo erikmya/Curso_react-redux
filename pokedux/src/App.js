30ef7d27f7a528fae92af30f23a94e99a5fd4ee6
erikmya <erik.maya@librerialeo.com.mx>
2022-10-17 13:31:17 -0500

    Clase 7 Introduccion a PokeAPI

--- pokedux/src/App.js
+++ pokedux/src/App.js
@@ -1,16 +1,33 @@
+import { useEffect, useState} from 'react';
 import { Col } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './PokemonList';
+import logo from './components/statics/logo.svg';
+import { getPokemons } from './api';
 import './App.css';
 
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemonsRes = await getPokemons();
+      setPokemons(pokemonsRes);
+    };
+
+    fetchPokemons();
+  }, []);
+
   return ( 
     <div className="App">
+      <Col span={4} offset={10}>
+      <img src={logo} alt='Pokedux' /> 
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList />
+      <PokemonList pokemons={pokemons} />
     </div>
    );
 }
--- pokedux/src/PokemonCard.jsx
+++ pokedux/src/PokemonCard.jsx
@@ -3,13 +3,18 @@ import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import './PokemonList.css';
 
-const PokemonCard = () => {
+const PokemonCard = ({ name }) => {
   return (
     <Card 
-        title="Ditto" 
-        cover={<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png' alt= "Ditto" />}
+    title={name}
+    cover={
+      <img
+        src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png'
+        alt='Ditto'
+      />
+    }
         extra={<StarOutlined />}
-    >
+        >
         <Meta description= 'fire, magic'/>
     </Card>
   );
