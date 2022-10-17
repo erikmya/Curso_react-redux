7d6ff24a8d963d61001f6284cf24f7b91f569a3d
erikmya <erik.maya@librerialeo.com.mx>
2022-10-17 14:10:20 -0500

    Clase 8 Integrando Redux

--- pokedux/src/App.js
+++ pokedux/src/App.js
@@ -1,15 +1,15 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import { Col } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './PokemonList';
 import logo from './components/statics/logo.svg';
 import { getPokemons } from './api';
+import { setPokemons as setPokemonsActions } from './actions';
 import './App.css';
 
 
-function App() {
-  const [pokemons, setPokemons] = useState([]);
-
+function App({ pokemons, setPokemons }) {
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemons();
@@ -32,4 +32,12 @@ function App() {
    );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
