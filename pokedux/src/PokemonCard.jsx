59ed86b6565f57c1f9900a3f0b316019a06af324
erikmya <erik.maya@librerialeo.com.mx>
2022-10-18 13:54:01 -0500

    Clase 16 Favoritos

--- pokedux/src/PokemonCard.jsx
+++ pokedux/src/PokemonCard.jsx
@@ -1,21 +1,26 @@
-import { StarOutlined } from '@ant-design/icons';
 import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
+import { useDispatch } from 'react-redux';
+import { setFavorite } from './actions';
+import StarButton from './components/StarButton';
 import './PokemonList.css';
 
-const PokemonCard = ({ name, image }) => {
+const PokemonCard = ({ name, image, types, id, favorite }) => {
+  const dispatch = useDispatch(); 
+  const typesString =  types.map(elem => elem.type.name).join(', ')
+;
+
+const handleOnFavorite = () => {
+  dispatch(setFavorite({ pokemonId: id }));
+};
+
   return (
     <Card 
     title={name}
-    cover={
-      <img
-        src={image}
-        alt={name}
-      />
-    }
-        extra={<StarOutlined />}
+    cover={<img src={image} alt={name}/>}
+        extra={<StarButton isFavorite={favorite} onClick={handleOnFavorite}/>}
         >
-        <Meta description= 'fire, magic'/>
+        <Meta description= {typesString}/>
     </Card>
   );
 };
